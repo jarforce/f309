faa49884d553b2602230e3f486f3aa47321fcdc8
Jarforce <35603960+jarforce@users.noreply.github.com>
2025-12-25 13:38:01 +0700

    Update Headlines.js

--- js/Headlines.js
+++ js/Headlines.js
@@ -865,7 +865,7 @@ const Headlines = {
 				this.initHeadlinesMenu();
 
 				if (Feeds.infscroll_disabled) {
-					hsp.innerHTML = "<a onclick='window.location.reload()'>" +
+					hsp.innerHTML = "<a href='#' onclick='Feeds.catchupFeed(this.getParent().row_id)'>" +
 						__("Click to open next unread feed.") + "</a>";						
 				}
 
@@ -877,10 +877,10 @@ const Headlines = {
 
 				if (hsp) {
 					if (first_id_changed) {
-						hsp.innerHTML = "<a onclick='window.location.reload()'>" +
+						hsp.innerHTML = "<a href='#' onclick='Feeds.catchupFeed(this.getParent().row_id)'>" +
 							__("New articles found, reload feed to continue.") + "</a>";
 					} else {
-						hsp.innerHTML = "<a onclick='window.location.reload()'>" +
+						hsp.innerHTML = "<a href='#' onclick='Feeds.catchupFeed(this.getParent().row_id)'>" +
 							__("Click to open next unread feed.") + "</a>";	
 					}
 				}
