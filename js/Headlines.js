b6d9e9264477550e225e1792ca6b5e5cbd3d3693
Jarforce <35603960+jarforce@users.noreply.github.com>
2025-12-25 13:41:51 +0700

    Update Headlines.js

--- js/Headlines.js
+++ js/Headlines.js
@@ -812,10 +812,8 @@ const Headlines = {
 				this.initHeadlinesMenu();
 
 				if (Feeds.infscroll_disabled)
-					//hsp.innerHTML = "<a onclick='window.location.reload()'>" +
-					//	__("Click to open next unread feed.") + "</a>";					
-					hsp.innerHTML = "<a href='#' onclick='Feeds.catchupFeed()'>" +
-						__("Click to open next unread feed.") + "</a>";
+					hsp.innerHTML = "<a onclick='window.location.reload()'>" +
+						__("Click to open next unread feed.") + "</a>";		
 				/*
 				if (Feeds._search_query) {
 					document.getElementById("feed_title").innerHTML += "<span id='cancel_search'>" +
@@ -865,7 +863,7 @@ const Headlines = {
 				this.initHeadlinesMenu();
 
 				if (Feeds.infscroll_disabled) {
-					hsp.innerHTML = "<a href='#' onclick='Feeds.catchupFeed()'>" +
+					hsp.innerHTML = "<a onclick='window.location.reload()'>" +
 						__("Click to open next unread feed.") + "</a>";						
 				}
 
@@ -877,10 +875,10 @@ const Headlines = {
 
 				if (hsp) {
 					if (first_id_changed) {
-						hsp.innerHTML = "<a href='#' onclick='Feeds.catchupFeed()'>" +
+						hsp.innerHTML = "<a onclick='window.location.reload()'>" +
 							__("New articles found, reload feed to continue.") + "</a>";
 					} else {
-						hsp.innerHTML = "<a href='#' onclick='Feeds.catchupFeed()'>" +
+						hsp.innerHTML = "<a onclick='window.location.reload()'>" +
 							__("Click to open next unread feed.") + "</a>";	
 					}
 				}
