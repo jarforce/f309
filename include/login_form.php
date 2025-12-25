111bc7af9f04e58a7294cdf3d950a54da23c6cc3
Jarforce <35603960+jarforce@users.noreply.github.com>
2025-12-25 19:39:17 +0700

    Update login_form.php

--- include/login_form.php
+++ include/login_form.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>F309 : Login</title>
+	<title>F309</title>
 	<link rel="shortcut icon" type="image/png" href="images/favicon.png">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<?php
